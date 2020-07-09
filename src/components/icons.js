e37f6660776075e593d03b37efe62818e2ccdfe6
Greg Donovan <gvdonovan@gmail.com>
2020-07-09 17:11:10 -0500

    moved svg's to icons.js

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+const BELL = () => {
+    return (
+        <svg
+            className="h-6 w-6"
+            stroke="currentColor"
+            fill="none"
+            viewBox="0 0 24 24"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+            />
+        </svg>
+    );
+};
+
 const FACEBOOK = () => {
     return (
         <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
@@ -23,6 +41,18 @@ const INSTAGRAM = () => {
         </svg>
     );
 };
+
+const SEARCH = () => {
+    return (
+        <svg className="h-5 w-5" fill="currentColor" viewBox="0 0 20 20">
+            <path
+                fillRule="evenodd"
+                clipRule="evenodd"
+                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
+            />
+        </svg>
+    );
+};
 const TWITTER = () => {
     return (
         <svg className="h-6 w-6" fill="currentColor" viewBox="0 0 24 24">
@@ -32,7 +62,9 @@ const TWITTER = () => {
 };
 
 export const Icons = {
+    BELL,
     FACEBOOK,
     INSTAGRAM,
+    SEARCH,
     TWITTER,
 };
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import UserProfileMenu from './UserProfileMenu';
 import icon from '../../assets/img/isn-logo-footer.png';
+import { Icons } from '../icons';
 
 const header = (props) => {
     return (
@@ -21,17 +22,7 @@ const header = (props) => {
                         </label>
                         <div className="relative w-full text-gray-400 focus-within:text-gray-600">
                             <div className="absolute inset-y-0 left-0 flex items-center pointer-events-none">
-                                <svg
-                                    className="h-5 w-5"
-                                    fill="currentColor"
-                                    viewBox="0 0 20 20"
-                                >
-                                    <path
-                                        fillRule="evenodd"
-                                        clipRule="evenodd"
-                                        d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                                    />
-                                </svg>
+                                <Icons.SEARCH />
                             </div>
                             <input
                                 id="search_field"
@@ -44,19 +35,7 @@ const header = (props) => {
 
                 <div className="ml-4 flex items-center md:ml-6">
                     <button className="p-1 text-gray-400 rounded-full hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:shadow-outline focus:text-gray-500">
-                        <svg
-                            className="h-6 w-6"
-                            stroke="currentColor"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-                            />
-                        </svg>
+                        <Icons.BELL />
                     </button>
                     <UserProfileMenu />
                 </div>
