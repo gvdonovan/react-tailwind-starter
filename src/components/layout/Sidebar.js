0e68073b2bd6efea45bbf43f0f1a0919cfd0f406
Greg Donovan <gvdonovan@gmail.com>
2020-07-11 02:49:45 -0500

    tweaks

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -10,7 +10,7 @@ const links = [
         exact: true,
     },
     {
-        name: 'Colors',
+        name: 'Color Palette',
         to: '/colors',
         exact: true,
     },
@@ -37,16 +37,15 @@ const Sidebar = () => {
     const className = 'flex items-center py-2 text-isn-gray-100';
     const activeClassName =
         'text-white bg-isn-gray-200 focus:outline-none focus:text-white';
-    // const className =
-    //     'mt-1 mx-2 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-white rounded-md hover:text-white hover:bg-isn-violet-400 focus:outline-none focus:text-white focus:bg-isn-violet-400 transition ease-in-out duration-150';
-    // const activeClassName =
-    //     'text-white bg-isn-violet-400 rounded-md focus:outline-none focus:text-white';
 
     return (
         <div className="hidden md:flex md:flex-shrink-0">
             <div className="flex flex-col w-64">
                 <div className="flex items-center h-16 flex-shrink-0 px-4 bg-isn-gray-100 shadow">
                     <img className="h-8 w-auto" src={logo} alt="isn" />
+                    <div className="px-2 text-isn-orange-400 text-lg font-semibold">
+                        React Toolkit
+                    </div>
                 </div>
                 <div className="h-0 flex-1 flex flex-col overflow-y-auto bg-gradient-b-isn-gray-dark">
                     <nav className="flex-1">
