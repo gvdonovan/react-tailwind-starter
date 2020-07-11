6113c4cd1156c983a40182b5579e8ab180e2c082
Greg Donovan <gvdonovan@gmail.com>
2020-07-11 02:33:51 -0500

    tweaks to nav colors

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -3,7 +3,7 @@ import { Icons } from '../icons';
 
 const footer = () => {
     return (
-        <div className="bg-white invisible md:visible bg-gradient-r-light">
+        <div className="invisible md:visible bg-isn-gray-100">
             <div className=" w-full py-4 px-4 flex justify-between flex-shrink">
                 <div className="flex justify-center md:order-2 invisible md:visible">
                     <a href="#" className="text-gray-400 hover:text-gray-500">
@@ -26,7 +26,7 @@ const footer = () => {
                     </a>
                 </div>
                 <div className="mt-8 md:mt-0 md:order-1">
-                    <p className="text-center leading-6 text-sm text-gray-300 font-medium">
+                    <p className="text-center leading-6 text-sm text-isn-gray-300 font-medium">
                         &copy; 2020 ISN
                     </p>
                 </div>
--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavLink, useRouteMatch } from 'react-router-dom';
 import logo from '../../assets/img/isn-logo-footer.png';
 
+//TODO: links should be passed in based on a UAC
 const links = [
     {
         name: 'Home',
@@ -13,15 +14,33 @@ const links = [
         to: '/colors',
         exact: true,
     },
+    {
+        name: 'Link1',
+        to: '/l1',
+        exact: true,
+    },
+    {
+        name: 'Link2',
+        to: '/l2',
+        exact: true,
+    },
+    {
+        name: 'Link3',
+        to: '/l3',
+        exact: true,
+    },
 ];
 
 const Sidebar = () => {
     const { url } = useRouteMatch();
 
-    const className =
-        'mt-1 mx-2 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-white rounded-md hover:text-white hover:bg-isn-violet-400 focus:outline-none focus:text-white focus:bg-isn-violet-400 transition ease-in-out duration-150';
+    const className = 'flex items-center py-2 text-isn-gray-100';
     const activeClassName =
-        'text-white bg-isn-violet-400 rounded-md focus:outline-none focus:text-white';
+        'text-white bg-isn-gray-200 focus:outline-none focus:text-white';
+    // const className =
+    //     'mt-1 mx-2 group flex items-center px-2 py-2 text-sm leading-5 font-medium text-white rounded-md hover:text-white hover:bg-isn-violet-400 focus:outline-none focus:text-white focus:bg-isn-violet-400 transition ease-in-out duration-150';
+    // const activeClassName =
+    //     'text-white bg-isn-violet-400 rounded-md focus:outline-none focus:text-white';
 
     return (
         <div className="hidden md:flex md:flex-shrink-0">
@@ -29,8 +48,8 @@ const Sidebar = () => {
                 <div className="flex items-center h-16 flex-shrink-0 px-4 bg-isn-gray-100 shadow">
                     <img className="h-8 w-auto" src={logo} alt="isn" />
                 </div>
-                <div className="h-0 flex-1 flex flex-col overflow-y-auto bg-isn-gray-300">
-                    <nav className="flex-1 px-2 py-4">
+                <div className="h-0 flex-1 flex flex-col overflow-y-auto bg-gradient-b-isn-gray-dark">
+                    <nav className="flex-1">
                         {links.map((route, index) => {
                             return (
                                 <NavLink
